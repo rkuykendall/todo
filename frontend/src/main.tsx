8521eccedc2637fb014c6c4dad7325967ba0ffda
Robert Kuykendall <robert@rkuykendall.com>
2025-04-21 15:53:20 -0500

    Upgrade all libraries

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import '@ant-design/v5-patch-for-react-19';
 import store from './store';
 import 'antd/dist/reset.css';
 import './index.css';
